4fffcdec35835d900fdfe48f9352af7c05961126
celia0325 <celiajnewell@gmail.com>
2022-11-22 12:01:47 -0600

    add

--- sketch.js
+++ sketch.js
@@ -5,11 +5,25 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let groundImg;
+
+function preload() {
+  groundImg = loadImage("ground.png");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(220);
+  background(color(0, 150, 255));
+  displayGround();
 }
+
+function displayGround() {
+  for (let i = 0; i < width; i+=100) {
+    image(groundImg, i, height+100, 100, 100);
+  }
+}
+
+
