3720641f9d1a13021d8ed05aedeb0f85b12f5510
celia0325 <celiajnewell@gmail.com>
2022-11-22 20:26:21 -0600

    fixed

--- sketch.js
+++ sketch.js
@@ -6,31 +6,7 @@
 // - describe what you did to take this project "above and beyond"
 
 let groundImg;
-<<<<<<< HEAD
 let groundBlocks =[];
-
-class Ground {
-  constructor() {
-    this.width = windowWidth;
-    this.height = windowHeight;
-    this.size = 50;
-    this.x;
-    this.y = this.height-this.size;
-  }
-
-  display() {
-    for (let x = 0; x < this.width; x+=50) {
-      image(groundImg, x, this.y, this.size, this.size)
-    }
-  }
-}
-
-let mario = {
-  speed: 5,
-  size: 60,
-  x : 30,
-  y : 460,
-}
 
 //right mario sprite
 let rStand;
@@ -47,9 +23,33 @@ let lHit;
 let img;
 let standImg = "R";
 
-function preload() {
-  groundImg = loadImage("ground.png")
+class Ground {
+  constructor() {
+    this.width = windowWidth;
+    this.height = windowHeight;
+    this.size = 50;
+    this.x;
+    this.y = height - this.size;
+  }
 
+  display() {
+    for (let x = 0; x < this.width; x+=50) {
+      image(groundImg, x, this.y, this.size, this.size)
+    }
+  }
+}
+
+let mario = {
+  jump : 0,
+  speed: 5,
+  size: 60,
+  x : 30,
+  y : 0,
+}
+
+function preload() {
+  groundImg = loadImage("ground.png");
+  
   rStand =  loadImage("mario-right.png");
   rWalk =  loadImage("mario-r-walk.png");
   rJump =  loadImage("mario-r-jump.png");
@@ -59,29 +59,24 @@ function preload() {
   lWalk =  loadImage("mario-l-walk.png");
   lJump =  loadImage("mario-l-jump.png");
   lHit =  loadImage("mario-l-hit.png");
-=======
-
-function preload() {
-  groundImg = loadImage("ground.png");
->>>>>>> 523603f2746384c9e27e0b8b796c888d846eea57
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  mario.jump = height - 50 - mario.size;
+  mario.y = height - 50 - mario.size;
   img = rStand;
   let block = new Ground
   groundBlocks.push(block);
 }
 
 function draw() {
-<<<<<<< HEAD
   background(color(0, 125, 250));
   for (let i = 0; i < groundBlocks.length; i++) {
     groundBlocks[i].display();
   }
-  image(img, mario.x, mario.y, mario.size/1.7, mario.size);
-  handleKeys();
-  
+  image(img, mario.x, mario.y, mario.size/1.7, mario.size)
+  handleKeys();  
 }
 
 function handleKeys() {
@@ -115,20 +110,10 @@ function handleKeys() {
 function keyPressed() {
   if (keyCode === UP_ARROW) {
    marioJumps();
-=======
-  background(color(0, 150, 255));
-  displayGround();
-}
-
-function displayGround() {
-  for (let i = 0; i < width; i+=100) {
-    image(groundImg, i, height+100, 100, 100);
->>>>>>> 523603f2746384c9e27e0b8b796c888d846eea57
   }
 }
 
 
-<<<<<<< HEAD
 function marioJumps() {
   if (mario.y >= 410) {
     while (mario.y >= 410) {
@@ -147,6 +132,3 @@ function marioJumps() {
     }
   }
 }
-  
-=======
->>>>>>> 523603f2746384c9e27e0b8b796c888d846eea57
