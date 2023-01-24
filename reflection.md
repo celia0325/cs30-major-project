2af4a5865497a0614be9e1d51e68f399825bfbcd
celia0325 <celiajnewell@gmail.com>
2023-01-24 12:08:26 -0600

    adapting final pieces

---
+++ reflection.md
@@ -0,0 +1,14 @@
+# Proposal
+
+Mario Bros Copy
+
+## Needs
+
+- gravity + jumping
+- collision with enemies
+- collision with coins / power ups
+- movement
+
+## Wants
+- customizable terrain
+- level/s
--- sketch.js
+++ sketch.js
@@ -1,27 +1,36 @@
+/* eslint-disable no-undef */
+// Celia Newell
+// November 19, 2022
+
+
 let mario;
 let walk, stand, jump;
 let littlem, tinyj;
 
-let cellSize;
+let cellSize = 50;
 let direction = "right";
 
+// enemies, would like more
 let  enemy, enemies, goomba;
 
+// building pieces
 let ground, blocks, block;
-// eslint-disable-next-line no-redeclare
 let mystery_box, boxes, box;
 let aBrick, bricks, brick;
 
+// coins and powerups
 let powerUp_ani, powerUp;
-let coin, coin_ani; // coins
-let needcoin = true;
+let coin, coin_ani;
 
 let flag, flag_pole;
 let cloud, clouds;
 
 let numOfB;
+let ROWS;
+// if mario collides with enemy
 let mario_hit = false;
   
+// loaads sprite animation images
 function preload() {
   clouds = loadAnimation("pieces/cloud.png");
   flag = loadAnimation("pieces/marioflag.png");
@@ -43,21 +52,21 @@ function preload() {
 }
 
 function setup() {
+  // drawing clouds
   for (let c = -3; c < 15; c++) {
     cloud = new Sprite(c*300, random(250, height-200));
     cloud.collider = "s";
     cloud.addAni("cloud", clouds);
     cloud.ani.scale = 0.25;
-    cloud.height = 10
+    cloud.height = 10;
   }
 
 
   imageMode(CENTER);
   createCanvas(windowWidth, windowHeight);
-  cellSize = 50;
   ROWS = height/cellSize-0.78;  
   
-  // start sprite in the center of the screen
+  // make and add mario animation
   mario = new Sprite();
   mario.collider = "k";
   
@@ -72,17 +81,18 @@ function setup() {
   mario.y = height/3;
   mario.height = 65;
   mario.width = 35;
-  
-  
-  /// blocks
-  blocks = new Group();
-  blocks.w = cellSize-1;
-  blocks.h = cellSize-0.25;
 
+  // make enemy
   enemies = new Group();
   enemies.w = 40;
   enemies.h = 35;
   enemies.visible = false;
+  
+  
+  // making groups for blocks, mystery boxes and bricks
+  blocks = new Group();
+  blocks.w = cellSize-1;
+  blocks.h = cellSize-0.25;
 
   boxes = new Group();
   boxes.w = cellSize;
@@ -92,7 +102,7 @@ function setup() {
   bricks.w = cellSize;
   bricks.h = cellSize-0.25;
 
-
+  // making coin, powerup and end flag pole 
   coin = new Sprite(0,0);
   coin.static = true;
   coin.d = 0.1;
@@ -113,34 +123,33 @@ function setup() {
   flag_pole.ani.scale = 0.5;
   flag_pole.w = 10;
 
-  make_blocks(-8, 1, 5);
-  
+  //makes new blocks and places them
   make_blocks(-8, 10, 50);
   make_blocks(-8, 9, 1);
   block.visible = false;
   make_blocks(-8, 8, 1);
   block.visible = false;
 
+  // makes and places a new brick and a new mystery box
   make_box(7,7);
   make_brick(6,7);
 }
   
-  
 function draw() {
+  // keeps sprites upright
   mario.rotation = 0;
   block.rotation = 0;
   powerUp.rotation = 0;
   enemies.rotation = 0;
   
   background(color(0, 125, 250));
-  
   loopFuctions();
 }
   
 function make_blocks(x, y, numOfB) {
+  // adaptable to make future improvements like custom levels
   for (let n = 1; n < numOfB+1; n++) {
     block = new blocks.Sprite();
-    
     block.addAni("ground", ground);
     block.collider = "k";
     
@@ -148,13 +157,13 @@ function make_blocks(x, y, numOfB) {
     block.ani.scale = 0.88;
     
     block.x = (x+n) * blocks.w-cellSize/2;
-
     block.y = height- (ROWS-y)*block.h-15;
   }
   
 }
 
 function make_brick(x, y) {
+  // adaptable to make future improvements like custom levels
   brick = new bricks.Sprite();
   brick.addAni("coin brick", aBrick);
   brick.ani.scale = 0.09;
@@ -166,6 +175,7 @@ function make_brick(x, y) {
 }
 
 function make_box(x, y) {
+  // adaptable to make future improvements like custom levels
   box = new boxes.Sprite();
   box.addAni("mystery box", mystery_box);
   box.ani.scale = 0.7;
@@ -174,6 +184,7 @@ function make_box(x, y) {
   box.y = height-((ROWS-y)*box.h-15);
 }
   
+// places goomba when mouse is pressed at mouse x and the last placed block's y
 function mousePressed() {
   goomba = new enemies.Sprite(mouse.x, block.y);
   goomba.addAni("goomba", enemy);
@@ -181,12 +192,14 @@ function mousePressed() {
   enemies.visible = true;
 }
 
+// all events needed to repeat in draw
 function loopFuctions() {
   mario_move();
-  camera.x = mario.x;
+  camera.x = mario.x; // allows screen to follow mario's movements
   if (block.y >= height-15) {
     blocks.collider = "s";
   }
+  // mario's gravity dependent on collision with blocks
   if (mario.colliding(blocks) > 2) {
     world.gravity.y = 10;
     mario.static = false;
@@ -197,43 +210,47 @@ function loopFuctions() {
     world.gravity.y = 20;
   }
 
+  // all collisions
   if (mario.colliding(powerUp) > 0) {
     powerUp.remove();
     mario_hit = false;
     console.log(mario_hit);
   }
 
+  if (enemies.visible === true) {
+    goomba.vel.x = -2;
+  }
+
+  // removes coin when mario touches it
   if (mario.overlaps(coin)) {
     coin.remove();
   }
 
-
+  // moves coin and makes it visible
   if (mario.colliding(bricks) > 0) {
     coin.visible = true;
     coin.x = brick.x;
     coin.y = brick.y-cellSize;
   }
 
+  // moves powerup and makes it visible
   if (mario.colliding(boxes) > 0) {
     powerUp.visible = true;
     powerUp.x = box.x;
     powerUp.y = box.y-cellSize;
-    //needcoin = false;
   }
+  // shrinks mario if hit by enemy
+  if (mario.collides(enemies)){
+    mario_hit = true;
+  }
+  // moves powerup only when visible
   if (powerUp.visible === true) {
     powerUp.dynamic = true;
     powerUp.vel.x = 2.9;
   }
-  if (mario.collides(enemies)){
-    mario_hit = true;
-  }
-
-  if (enemies.visible === true) {
-    enemies.vel.x = -2;
-  }
-  mario_move();
 }
 
+// sets mario's movements and sprite animation based on collision and keyboard commands
 function mario_move(){
   if (mario_hit === false){
     mario.ani = "walking";
@@ -241,7 +258,7 @@ function mario_move(){
   }
   else {
     mario.ani = "shrink";
-    mario.h = 40;
+    mario.h = 38;
   }
   if (kb.pressed("up")) {
     if (direction === "left") {
